1adacbcc7fd444aa6f2f85093d174860a8cc6431
Michał Łaszczewski <michal@laszczewski.com>
2021-05-14 07:48:49 +0200

    cache, global app

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/App.js')
+const App = require('./lib/App.js')
+App.app = () => {
+  if(!global.liveChangeFrameworkApp) {
+    global.liveChangeFrameworkApp = new App()
+  }
+  return global.liveChangeFrameworkApp
+}
+module.exports = App
