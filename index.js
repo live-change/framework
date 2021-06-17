ad0840be37471e8b7867a51639419fae7cb2f345
Michał Łaszczewski <michal@laszczewski.com>
2021-06-17 11:59:43 +0200

    Definitions exported

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ App.app = () => {
   }
   return global.liveChangeFrameworkApp
 }
+
 module.exports = App
+
+module.exports.ActionDefinition = require('./lib/definition/ActionDefinition.js')
+module.exports.EventDefinition = require('./lib/definition/EventDefinition.js')
+module.exports.ForeignIndexDefinition = require('./lib/definition/ForeignIndexDefinition.js')
+module.exports.ForeignModelDefinition = require('./lib/definition/ForeignModelDefinition.js')
+module.exports.IndexDefinition = require('./lib/definition/IndexDefinition.js')
+module.exports.ModelDefinition = require('./lib/definition/ModelDefinition.js')
+module.exports.PropertyDefinition = require('./lib/definition/PropertyDefinition.js')
+module.exports.ServiceDefinition = require('./lib/definition/ServiceDefinition.js')
+module.exports.TriggerDefinition = require('./lib/definition/TriggerDefinition.js')
+module.exports.ViewDefinition = require('./lib/definition/ViewDefinition.js')
+
